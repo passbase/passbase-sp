fde781711014ed436cab6fa1bf0fa2740cd5b0c3
Skorostetskyi Sergii <sergii@skorostetskyi.com>
2021-07-09 16:28:29 +0200

    Release 2.7.3

--- Package.swift
+++ Package.swift
@@ -10,13 +10,10 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "Passbase",
-                      url: "https://button.passbase.com/__ios/Passbase_2.7.0.zip",
-                      checksum: "cffef8c47c7c67c8e60d73f7eb919f249e06251facdfe259695ebd33efb93bbb"),
+                      url: "https://button.passbase.com/__ios/Passbase_2.7.3.zip",
+                      checksum: "2f28e5bf588478eab687c8c9132b3649ccbf82245acd4c2931b7d113d27704e9"),
         .binaryTarget(name: "Microblink",
                       url: "https://button.passbase.com/__ios/Microblink_5.11.0.zip",
                       checksum: "4583840c2d3cf926830943979c8efc77386265547c0a352d20086fffc2e9ca93"),
-        .binaryTarget(name: "ZoomAuthentication",
-                      url: "https://button.passbase.com/__zoom/ZoomAuthentication-ios-8.7.2.zip",
-                      checksum: "c57bb18f9a05eb10fa6294c4c495532d28aa30e81797e800178062bfad9119a6"),
     ]
 )
