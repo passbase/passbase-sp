c2af773708862037323cd24386e5bacc6a41e47d
Skorostetskyi Sergii <sergii@skorostetskyi.com>
2021-10-18 17:35:10 +0200

    Release 2.8.3

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "Passbase",
-                      url: "https://button.passbase.com/__ios/Passbase_2.8.2.zip",
-                      checksum: "fb0b5945b332e6a92bee3858d3b883266e79747e768ba743ddb372440eb4e108"),
+                      url: "https://button.passbase.com/__ios/Passbase_2.8.3.zip",
+                      checksum: "ceaf1aa04c91ff5ab776564a0511428ce9650fd594fd42e4ba49893eab272753"),
         .binaryTarget(name: "Microblink",
                       url: "https://button.passbase.com/__ios/Microblink_5.11.0.zip",
                       checksum: "4583840c2d3cf926830943979c8efc77386265547c0a352d20086fffc2e9ca93"),
