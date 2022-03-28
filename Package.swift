b4f731b0de3c0c14e51ef2d4055a82e044db02c7
Skorostetskyi Sergii <sergii@skorostetskyi.com>
2022-03-28 23:48:54 +0200

    fix: framework source path

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
                       url: "https://button.passbase.com/__ios/Passbase_2.12.1.zip",
                       checksum: "ac81a89835742a627d216593bed8c1d7d2de773799d028edda5c47999edac71a"),
         .binaryTarget(name: "Microblink",
-                      url: "https://button.passbase.com/__ios/Microblink_5.16.0.zip",
+                      url: "https://button.passbase.com/__ios/Microblink_5.16.1.zip",
                       checksum: "33af45af2820e2bc0ff929ade3714c09c923ed03a1606d5219e16b9f87ddeeed"),
     ]
 )
